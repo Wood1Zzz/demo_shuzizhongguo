0065c086a315af368b77daf9b84d0d1db1421e2e
Wood1Zzz <455349235@qq.com>
2026-03-12 07:54:06 +0800

    change

--- resizeMask.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-
-imgpath = '/home/danzer/Documents/code/demo_shuzizhongguo/ForgeryAnalysis_Stage_1_Test/Image/234d916686d644e580f3de4752feecf8.jpg'
-img = Image.open(imgpath)
-print(img.size)  # 输出原始图像尺寸
